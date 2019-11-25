7848fa0aa1af4e7f59b580279b13923e7224bdcc
Xeon <nickandreyuser@gmail.com>
2019-11-25 18:26:25 +0100

    add fetch from fake db. change db.json structure.

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,10 +1,19 @@
 (function() {
   /*graphic, illustration, motion*/
-  const filter = document.querySelector('.portfolio__filter');
-  const controls = filter.querySelector('.portfolio__filter__controls');
-  const filterItems = filter.querySelector('.portfolio__filter__items');
+  // const filter = document.querySelector('.portfolio__filter');
+  // const controls = filter.querySelector('.portfolio__filter__controls');
+  // const filterItems = filter.querySelector('.portfolio__filter__items');
+  //
+  // filterItems.querySelectorAll('[aria-label]').forEach((item) => {
+  //   console.log(item)
+  // });
 
-  filterItems.querySelectorAll('[aria-label]').forEach((item) => {
-    console.log(item)
-  });
+  fetch(`https://my-json-server.typicode.com/nickAndrey/webpaint/db`)
+    .then((resp) => resp.json())
+    .then((data) => getImages(data))
+    .catch((err) => console.log(err));
+
+  const getImages = (data) => {
+
+  }
 })();
